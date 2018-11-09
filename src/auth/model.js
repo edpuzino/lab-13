af313ce5d84ed25374a0bc23d6c41e5622d36a5d
edpuzino <edpuzino@yahoo.com>
2018-11-08 18:37:49 -0800

    finally got my lint working

--- src/auth/model.js
+++ src/auth/model.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  email: {type: String}
+  username: {type: String, require: true, unique: true},
+  password: {type: String, require: true},
+  email: {type: String},
 });
 
 // Before we save, use bcrypt to hash the plain text password and then call next()
@@ -27,12 +29,13 @@ userSchema.pre('save', function(next) {
 userSchema.statics.authenticateBasic = function(auth) {
   let query = {username:auth.username};
   return this.findOne(query)
-    .then(user => user && user.comparePassword(auth.password))
+    .then(user => {
+      return user && user.comparePassword(auth.password);})
     .catch(error => error);
 };
 
 userSchema.statics.authenticateToken = function(token) {
-  let parsedToken = jwt.verify(token);
+  let parsedToken = jwt.verify(token, process.env.SECRET || 'changeme');
   let query = {_id:parsedToken.id};
   return this.findOne(query)
     .then(user => {
@@ -43,7 +46,7 @@ userSchema.statics.authenticateToken = function(token) {
 
 // Compare a plain text password against the hashed one we have saved
 userSchema.methods.comparePassword = function(password) {
-  return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password).then( valid => valid ? this : null );
 };
 
 // Generate a JWT from the user id and a secret
@@ -51,7 +54,7 @@ userSchema.methods.generateToken = function() {
   let tokenData = {
     id:this._id,
   };
-  return jwt.sign(tokenData, process.env.SECRET || 'changeit' );
+  return jwt.sign(tokenData, process.env.SECRET || 'changeme' );
 };
 
 export default mongoose.model('users', userSchema);
